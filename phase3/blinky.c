359c41116719477bdff80d2ef7cf797f7b7b6f35
Hung-I Wang <i@hung-i.wang>
2018-03-24 20:55:41 +0800

    asgmt0 finished

--- phase3/blinky.c
+++ phase3/blinky.c
@@ -16,5 +16,12 @@ static void spin_sleep_ms(unsigned int ms) {
 
 int main(void) {
   // STEP 1: Set GPIO Pin 16 as output.
+  *GPIO_FSEL1 = (*GPIO_FSEL1 | 0b111 << 18) & (0b001 << 18);
   // STEP 2: Continuously set and clear GPIO 16.
+  while (1) {
+    *GPIO_SET0 = 0b1 << 16;
+    spin_sleep_ms(100);
+    *GPIO_CLR0 = 0b1 << 16;
+    spin_sleep_ms(100);
+  }
 }
--- phase4/src/lib.rs
+++ phase4/src/lib.rs
@@ -22,5 +22,12 @@ fn spin_sleep_ms(ms: usize) {
 #[no_mangle]
 pub unsafe extern "C" fn kmain() {
     // STEP 1: Set GPIO Pin 16 as output.
+    GPIO_FSEL1.write_volatile((GPIO_FSEL1.read_volatile() & (0b111 << 18)) | (0b001 << 18));
     // STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        GPIO_SET0.write_volatile(0b1 << 16);
+        spin_sleep_ms(500);
+        GPIO_CLR0.write_volatile(0b1 << 16);
+        spin_sleep_ms(500);
+    }
 }
